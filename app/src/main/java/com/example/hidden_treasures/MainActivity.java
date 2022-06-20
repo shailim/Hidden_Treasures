9be5d1201e1febf337c02283286be00bdd417494
Shaili Manda <shailim@fb.com>
2022-06-20 12:12:50 -0700

    added layout and fragment files and bottom navigation bar

--- app/src/main/java/com/example/hidden_treasures/MainActivity.java
+++ app/src/main/java/com/example/hidden_treasures/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.hidden_treasures;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+
+        // handling navigation selection
+        bottomNavigationView.setOnItemSelectedListener(
+                new NavigationBarView.OnItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        Fragment fragment;
+                        switch (item.getItemId()) {
+                            case R.id.action_create:
+                                fragment = CreateFragment.newInstance();
+                                break;
+                            case R.id.action_profile:
+                                fragment = ProfileFragment.newInstance();
+                                break;
+                            default:
+                                fragment = MapFragment.newInstance();
+                                break;
+                        }
+                        fragmentManager.beginTransaction().replace(R.id.clContainer, fragment).commit();
+                        return true;
+                    }
+                });
+        // default tab is map
+        bottomNavigationView.setSelectedItemId(R.id.action_map);
     }
 }
---
+++ app/src/main/java/com/example/hidden_treasures/MarkerDetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.hidden_treasures;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MarkerDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_marker_detail);
+    }
+}
