f11959fa6db43fdbd0b1fa2c4f057225774cf2c5
Shaili Manda <shailim@fb.com>
2022-06-24 12:36:34 -0700

    Enable taking videos, buttons to choose photo or video

--- app/src/main/java/com/example/hidden_treasures/CreateFragment.java
+++ app/src/main/java/com/example/hidden_treasures/CreateFragment.java
@@ -1,19 +1,13 @@
 package com.example.hidden_treasures;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.core.content.FileProvider;
-import androidx.fragment.app.Fragment;
-
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,6 +15,17 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
+import androidx.fragment.app.Fragment;
 
 import java.io.File;
 
@@ -33,8 +38,11 @@ public class CreateFragment extends Fragment {
     private EditText etDescription;
     private Button btnSubmitMarker;
     private ImageView ivPreview;
+    private VideoView vvPreview;
+    private Button btnTakePicture;
+    private Button btnTakeVideo;
 
-    ActivityResultLauncher<Uri> resultLauncher = registerForActivityResult(
+    private ActivityResultLauncher<Uri> cameraLauncher = registerForActivityResult(
             new ActivityResultContracts.TakePicture(),
             new ActivityResultCallback<Boolean>() {
                 @Override
@@ -44,6 +52,9 @@ public class CreateFragment extends Fragment {
                         //decoding image
                         Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
 
+                        // make image preview visible
+                        ivPreview.setVisibility(View.VISIBLE);
+
                         //setting the image to the image preview in layout
                         ivPreview.setImageBitmap(takenImage);
                     } else {
@@ -51,6 +62,19 @@ public class CreateFragment extends Fragment {
                     }
                 }
             });
+
+    private final ActivityResultLauncher<Intent> videoLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+            if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
+                Uri contentURI = result.getData().getData();
+                vvPreview.setVisibility(View.VISIBLE);
+                vvPreview.setVideoURI(contentURI);
+                vvPreview.start();
+            }
+        }
+    });
+
 
     public CreateFragment() {
         // Required empty public constructor
@@ -71,24 +95,68 @@ public class CreateFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_create, container, false);
+        return inflater.inflate(R.layout.fragment_create, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
         // get references to all the views in layout
         ivPreview = (ImageView) view.findViewById(R.id.ivPreview);
+        vvPreview = (VideoView) view.findViewById(R.id.vvPreview);
         etTitle = view.findViewById(R.id.etTitle);
         etDescription = view.findViewById(R.id.etDescription);
         btnSubmitMarker = view.findViewById(R.id.btnSubmitMarker);
+        btnTakePicture = view.findViewById(R.id.btnTakePicture);
+        btnTakeVideo = view.findViewById(R.id.btnTakeVideo);
 
-        // getting a file reference
-        photoFile = new File(getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES), "share_image_" + System.currentTimeMillis() + ".png");
+        // set the onClick listeners for picture and video button
+        setButtonListeners();
+    }
 
-        // wrapping File object into a content provider
-        Uri fileProvider = FileProvider.getUriForFile(getContext(), "com.example.hidden_treasures.fileprovider", photoFile);
 
-        // launch intent to open camera
-        resultLauncher.launch(fileProvider);
+    /* Sets the onClickListeners for any buttons */
+    private void setButtonListeners() {
+        btnTakePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // make sure a previous photo or video taken is removed from the view
+                setMediaPreviewInvisible();
 
-        // return the view
-        return view;
+                // getting a file reference
+                photoFile = new File(getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES), "share_image_" + System.currentTimeMillis() + ".png");
+
+                // wrapping File object into a content provider
+                Uri fileProvider = FileProvider.getUriForFile(getContext(), "com.example.hidden_treasures.fileprovider", photoFile);
+
+                // launch intent to open camera
+                cameraLauncher.launch(fileProvider);
+            }
+        });
+
+        btnTakeVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // make sure a previous photo or video taken is removed from the view
+                setMediaPreviewInvisible();
+
+                // create intent to open video camera
+                Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+
+                // only launch intent if it can be handled
+                if (getContext().getPackageManager().resolveActivity(intent, 0) != null) {
+                    videoLauncher.launch(intent);
+                } else {
+                    Toast.makeText(getContext(), "No apps supports this action", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    /* Resets the image and video views to invisible whenever user takes a new picture/video*/
+    private void setMediaPreviewInvisible() {
+        ivPreview.setVisibility(View.INVISIBLE);
+        vvPreview.setVisibility(View.INVISIBLE);
     }
 }
