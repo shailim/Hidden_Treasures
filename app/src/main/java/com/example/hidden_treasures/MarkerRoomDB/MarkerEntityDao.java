39bfedd9f90d17a8e757231a37f2b981fd9088e1
Shaili Manda <shailim@fb.com>
2022-07-21 23:51:10 -0700

    fixed crash caused by unique constraint error

--- app/src/main/java/com/example/hidden_treasures/MarkerRoomDB/MarkerEntityDao.java
+++ app/src/main/java/com/example/hidden_treasures/MarkerRoomDB/MarkerEntityDao.java
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 import androidx.room.Update;
 
@@ -23,7 +24,7 @@ public interface MarkerEntityDao {
     @Insert
     void insertAll(MarkerEntity... markers);
 
-    @Insert
+    @Insert (onConflict = OnConflictStrategy.REPLACE)
     void insert(MarkerEntity marker);
 
     @Query("UPDATE marker_entity SET icon = :icon WHERE objectId = :id")
