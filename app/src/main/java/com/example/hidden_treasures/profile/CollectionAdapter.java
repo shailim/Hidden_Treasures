d66893df608e90ea3691203fb350ab1a3b888b88
Shaili Manda <shailim@fb.com>
2022-07-28 11:38:26 -0700

    Adapters for the profile and collection marker recycler views

--- app/src/main/java/com/example/hidden_treasures/profile/CollectionAdapter.java
+++ app/src/main/java/com/example/hidden_treasures/profile/CollectionAdapter.java
@@ -9,42 +9,39 @@ import android.widget.ImageView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.amazonaws.HttpMethod;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
 import com.bumptech.glide.Glide;
 import com.example.hidden_treasures.MarkerRoomDB.MarkerEntity;
 import com.example.hidden_treasures.R;
+import com.example.hidden_treasures.collections.ParseCollection;
+import com.example.hidden_treasures.markers.ParseMarker;
 import com.example.hidden_treasures.util.S3Helper;
 
 import org.greenrobot.eventbus.EventBus;
 
-import java.net.URL;
 import java.util.Date;
 import java.util.List;
 
-public class GridAdapter extends RecyclerView.Adapter<GridAdapter.ViewHolder> {
+public class CollectionAdapter extends RecyclerView.Adapter<CollectionAdapter.ViewHolder> {
 
-    List<MarkerEntity> markers;
+    List<ParseMarker> markers;
     Context context;
 
-    public GridAdapter(Context context, List<MarkerEntity> markers) {
+    public CollectionAdapter(Context context, List<ParseMarker> markers) {
         this.context = context;
         this.markers = markers;
     }
 
     @NonNull
     @Override
-    public GridAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public CollectionAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(context);
         View view = inflater.inflate(R.layout.grid_item, parent, false);
-        return new GridAdapter.ViewHolder(view);
+        return new CollectionAdapter.ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull GridAdapter.ViewHolder holder, int position) {
-        MarkerEntity marker = markers.get(position);
+    public void onBindViewHolder(@NonNull CollectionAdapter.ViewHolder holder, int position) {
+        ParseMarker marker = markers.get(position);
         holder.bind(marker);
     }
 
@@ -62,8 +59,8 @@ public class GridAdapter extends RecyclerView.Adapter<GridAdapter.ViewHolder> {
             ivMarkerImage = itemView.findViewById(R.id.ivMarkerImage);
         }
 
-        public void bind(MarkerEntity marker) {
-            String imageKey = marker.imageKey;
+        public void bind(ParseMarker marker) {
+            String imageKey = marker.getImage();
             // get a signed url for the image
             String url = S3Helper.getSignedUrl(context, imageKey).toString();
             Glide.with(context).load(url).into(ivMarkerImage);
@@ -71,7 +68,7 @@ public class GridAdapter extends RecyclerView.Adapter<GridAdapter.ViewHolder> {
                 @Override
                 public void onClick(View v) {
                     // posting new marker event
-                    EventBus.getDefault().post(new ImageClickEvent(imageKey, marker.title, marker.view_count, new Date(marker.createdAt)));
+                    EventBus.getDefault().post(new ImageClickEvent(imageKey, marker.getTitle(), marker.getViewCount(), new Date(marker.getTime())));
                 }
             });
         }
